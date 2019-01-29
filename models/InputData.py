c35f475e570cd986af89b1f519cb5a5abc8aeea9
Matt <matthew.hope396@outlook.com>
2019-01-29 16:29:52 +0000

    Added chatbot code.

---
+++ models/InputData.py
@@ -0,0 +1,12 @@
+class InputData:
+    def __init__(self):
+        self.num_lines = 0
+        self.chars = set()
+        self.num_tokens = 0
+        self.max_len = 0
+
+    def loadJSON(self, data):
+        self.num_lines = data['num_lines']
+        self.chars = data['chars']
+        self.num_tokens = data['num_tokens']
+        self.max_len = data['max_len']
---
+++ train.py
@@ -0,0 +1,68 @@
+# pylint: disable=E0401,E0611
+
+import os
+import json
+script_dir = os.path.dirname(__file__)
+
+from helpers.DataService import DataService
+from models.InputData import InputData
+from models.OutputData import OutputData
+from models.DataResult import DataResult
+from models.Encoder import Encoder
+from models.Decoder import Decoder
+from Config import BATCH_SIZE, EPOCHS
+from generator.TrainingGenerator import TrainingGenerator
+
+from tensorflow.keras.models import Model, save_model
+from tensorflow.keras.callbacks import LambdaCallback
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+# Process the dataset
+print('STARTING: loading_data')
+data_result = DataResult(None, None)
+
+with open(script_dir + './temp/processed_data.json', 'r') as output:
+    json_data = json.load(output)
+    data_result.loadJSON(json_data)
+print('END: loading_data')
+print('')
+
+# Create the encoder
+print('STARTING: create encoder')
+encoder = Encoder(data_result.input_data)
+print('END: create encoder')
+print('')
+
+# Create the decoder
+print('STARTING: create decoder')
+decoder = Decoder(data_result.output_data, encoder)
+print('STARTING: create decoder')
+print('')
+
+# Create the model
+print('STARTING: create model')
+model = Model([encoder.inputs, decoder.inputs], decoder.outputs)
+print('END: create model')
+print('')
+
+# Compile the model
+print('STARTING: compile model')
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+print('END: compile model')
+print('')
+
+# Train the model
+print('STARTING: train model')
+print(' Training with ' + str(data_result.input_data.num_lines) + ' lines')
+
+generator = TrainingGenerator(data_result, BATCH_SIZE)
+model.fit_generator(generator, epochs=EPOCHS, verbose=1)
+# model.fit([token_result.encoder_input, token_result.decoder_input], token_result.decoder_output, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_split=0.2)
+print('END: train model')
+print('')
+
+#Save the entire model
+save_model(model, 'model.h5')
+
+#Save the weights for cpu compatibility
+model.save_weights('model_weights.h5')
